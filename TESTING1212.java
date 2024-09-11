ab615ee306ba6bdd73a232959b6f43fb9890acf0
Thomas <jerrythomascraig3@gmail.com>
2024-09-11 13:35:19 -0400

    Testing now.

---
+++ TESTING1212.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class TESTING1212 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TESTING1212
+{
+    public static void main (String[] args)
+    {
+        System.out.println("TESTING TESTING 1-2 1-2");
+    }
+}
